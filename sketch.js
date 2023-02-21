51e6afda5bef22895d07438fabddbc12cf205884
camille-granier <granierc@protonmail.com>
2023-02-21 12:48:07 +1100

    base structure

--- sketch.js
+++ sketch.js
@@ -9,13 +9,21 @@ function draw() {
   rotateX(60)
   noFill()
   stroke(255);
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 50; i++) {
+
+    var r = map(sin(frameCount/2), -1, 1, 100, 200)
+    var g = map(i, 0, 50, 100, 200)
+    var b = map(cos(frameCount), -1, 1, 200, 100)
+
+    stroke(r, g, b)
+
+    rotate((frameCount / 20))
     beginShape();
-    for (let j = 0; j < 360; j += 10)  {
-      var rad = i * 8;
+    for (let j = 0; j < 360; j += 30)  {
+      var rad = i * 3;
       var x = rad * cos(j);
       var y = rad * sin(j);
-      var z = sin(frameCount) * 50
+      var z = sin(frameCount * 2+ i * 5) * 50
 
       vertex(x, y, z)
     }
